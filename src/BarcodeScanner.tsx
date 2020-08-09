e97365f744cb52aba6e29aa027b2f5457927851e
Deepank Singhal <deepank.singhal22@gmail.com>
2020-08-09 16:09:04 +0400

    Added Cropped

--- src/BarcodeScanner.tsx
+++ src/BarcodeScanner.tsx
@@ -1,10 +1,13 @@
-import React, { useRef, useLayoutEffect, useEffect } from 'react';
+import React, { useRef, useLayoutEffect } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import ZXing from './lib/zxing_reader';
 
 interface ScannerProps {
   onScan: Function;
 }
+
+let zxing: any;
+ZXing().then((instance: any) => (zxing = instance));
 
 const useStyles = makeStyles({
   root: {
@@ -19,7 +22,6 @@ const useStyles = makeStyles({
     alignItems: 'center',
     overflow: 'hidden',
     zIndex: -100,
-    // maxHeight: '517px',
   },
   scanVideo: {
     width: '100%',
@@ -37,15 +39,15 @@ const useStyles = makeStyles({
     transform: `translateZ(0)`,
     display: 'flex',
     alignItems: 'center',
-    left: `${(0.025 + 0.12 * 0.95) * 100}%`,
-    width: `${0.75 * 0.95 * 100}%`,
-    top: `${(0.025 + 0.4 * 0.95) * 100}%`,
-    height: `${0.1 * 0.95 * 100}%`,
-    // left: '13.9%',
-    // width: '71.25%',
-    // top: 40.5%',
+    left: '13.9%',
+    width: '71.25%',
+    top: '45.25%',
+    height: '9.5%',
+
+    // left: `${(0.025 + 0.12 * 0.95) * 100}%`,
+    // width: `${0.75 * 0.95 * 100}%`,
+    // top: `${(0.025 + 0.4 * 0.95) * 100}%`,
     // height: `${0.1 * 0.95 * 100}%`,
-    border: '1px solid',
   },
   laserImg: {
     position: 'absolute',
@@ -60,10 +62,6 @@ const useStyles = makeStyles({
     boxSizing: 'border-box',
     border: '1px solid red',
     transform: `translateZ(0) scale(-1,1)`,
-    // left: `${(0.025 + 0.12 * 0.95) * 100}%`,
-    // width: `${0.75 * 0.95 * 100}%`,
-    // top: `${(0.025 + 0.4 * 0.95) * 100}%`,
-    // height: `${0.1 * 0.95 * 100}%`,
   },
 });
 
@@ -72,14 +70,6 @@ const BarcodeScanner: React.FC<ScannerProps> = ({ onScan }) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const laserBoxRef = useRef<HTMLDivElement>(null);
   const classes = useStyles();
-  let zxing: any;
-
-  useEffect(() => {
-    async function init() {
-      zxing = await ZXing();
-    }
-    init();
-  });
 
   useLayoutEffect(() => {
     async function init() {
@@ -123,42 +113,30 @@ const BarcodeScanner: React.FC<ScannerProps> = ({ onScan }) => {
   }
 
   const tick = () => {
-    if (!canvasRef.current || !laserBoxRef.current || !scannerRef.current) {
+    if (!canvasRef.current || !scannerRef.current) {
       return;
     }
     if (scannerRef.current.readyState === scannerRef.current.HAVE_ENOUGH_DATA) {
-      const canvasDimensions = canvasRef.current.getBoundingClientRect();
+      canvasRef.current.height = scannerRef.current.videoHeight * 0.095;
+      canvasRef.current.width = scannerRef.current.videoWidth * 0.7125;
+
+      const xOffset = scannerRef.current.videoWidth * 0.139;
+      const yOffset = scannerRef.current.videoHeight * 0.4525;
+      const croppedWidth = scannerRef.current.videoWidth * 0.7125;
+      const croppedHeight = scannerRef.current.videoHeight * 0.095;
+
       const canvas = canvasRef.current.getContext('2d');
-
-      canvasRef.current.height = scannerRef.current.videoHeight;
-      canvasRef.current.width = scannerRef.current.videoWidth;
-
-      // console.log(
-      //   canvasDimensions.x,
-      //   canvasDimensions.y,
-      //   canvasDimensions.width,
-      //   canvasDimensions.height * 2
-      // );
-
-      // canvas!.drawImage(
-      //   scannerRef.current,
-      //   canvasDimensions.x,
-      //   canvasDimensions.y,
-      //   canvasDimensions.width * 3.1,
-      //   canvasDimensions.height * 3,
-      //   0,
-      //   0,
-      //   canvasDimensions.width,
-      //   canvasDimensions.height * 3
-      // );
       canvas!.drawImage(
         scannerRef.current,
+        xOffset,
+        yOffset,
+        croppedWidth,
+        croppedHeight,
         0,
         0,
-        canvasRef.current.width,
-        canvasRef.current.height
+        croppedWidth,
+        croppedHeight
       );
-      // canvas!.drawImage(scannerRef.current, 0, 0);
 
       const code = scanBarcode(canvasRef.current, 'CODE_39');
       if (code.error) {
@@ -174,7 +152,7 @@ const BarcodeScanner: React.FC<ScannerProps> = ({ onScan }) => {
 
   return (
     <div className={classes.container}>
-      <canvas ref={canvasRef} className={classes.canvas}></canvas>
+      <canvas ref={canvasRef} className={classes.canvas} hidden></canvas>
       <video
         ref={scannerRef}
         muted
@@ -187,7 +165,6 @@ const BarcodeScanner: React.FC<ScannerProps> = ({ onScan }) => {
           src="/assets/laser.png"
           alt="laser"
           className={classes.laserImg}
-          hidden
         ></img>
       </div>
     </div>
